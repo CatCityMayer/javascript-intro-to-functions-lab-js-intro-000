4154420cb73376e6884374b0e643c66747926ab0
CatCityMayor <chadtroy83@gmail.com>
2018-10-19 20:32:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
+function logShout() {
+  console.log(string.toUpperCase())
+}
+
 function logWhisper() {
   console.log(string.toLowerCase())
 }
